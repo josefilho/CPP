b8bd153865be8d8eb95d42c2ac6f64b72a976812
josefilho <iamjose.filho@gmail.com>
2022-02-06 18:47:11 -0400

    feat: Criando módulos com C++

---
+++ CriandoModulos/jMath.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lofyl on 04/02/2022.
+//
+
+int fatorial(int n){
+  return n == 1 ? 1: n * fatorial(n-1);
+}
+
+float areaQuadrado(float h){
+  return  h * h;
+}
+
+float areaRetangulo(float base, float altura){
+  return base * altura;
+}
---
+++ CriandoModulos/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "jMath.cpp"
+
+int main() {
+  std::cout << fatorial(5) << std::endl;
+  return 0;
+}
